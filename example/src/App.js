4b7249d745db6924d6b35f52c38344d65c2bbbe0
Gerardo Perez Soria <gerardoperezsoria@gmail.com>
2020-08-31 19:46:00 -0500

    resubida de codigo

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+// import React from 'react'
 
-import { ExampleComponent } from 'democomponents'
-import 'democomponents/dist/index.css'
+// import { ExampleComponent } from 'democomponents'
+// import 'democomponents/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+// const App = () => {
+//   return <ExampleComponent text="Create React Library Example 😄" />
+// }
+
+// export default App
+
+import React, { Component } from 'react'
+import {ColoredHeadingOne, ColoredHeadingTwo} from 'democomponents'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ColoredHeadingOne
+        text='Hello desde mi primer component'
+        color={'red'}
+        />
+
+      <ColoredHeadingTwo
+        text='Hello gps'
+        color={'#FFFF99'}
+        />
+      </div>
+    );
+  }
 }
-
-export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import React from 'react'
-import styles from './styles.module.css'
+// import React from 'react'
+// import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+// export const ExampleComponent = ({ text }) => {
+//   return <div className={styles.test}>Example Component: {text}</div>
+// }
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class ColoredHeadingTwo extends Component {
+  static propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
+  }
+
+  render() {
+    const { text, color } = this.props
+    return <h1 style={{ color }}> {text}</h1>
+  }
+}
+
+export class ColoredHeadingOne extends Component {
+  static propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
+  }
+
+  render() {
+    const { text, color } = this.props
+    return <h1 style={{ color }}> {text}</h1>
+  }
 }
